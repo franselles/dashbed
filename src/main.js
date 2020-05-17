854f8f35404e31135333d30635427f19627bf1b8
franselles <fran.selles@gmail.com>
2020-05-17 19:17:32 +0200

    First online

--- src/main.js
+++ src/main.js
@@ -10,8 +10,8 @@ import axios from 'axios';
 import VueAxios from 'vue-axios';
 Vue.use(VueAxios, axios);
 
-const baseURL = 'http://localhost:8080/api/v1';
-// const baseURL = '/api/v1';
+// const baseURL = 'http://localhost:8080/api/v1';
+const baseURL = '/api/v1';
 
 axios.defaults.baseURL = baseURL;
 axios.defaults.json = true;
