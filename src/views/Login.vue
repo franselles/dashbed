9026bc65d8ce4d873ea49782bacb43823bf5b068
franselles <fran.selles@gmail.com>
2020-05-17 23:12:33 +0200

    PWA update test 1

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -16,7 +16,7 @@
       <div class="control">
         <b-taglist attached>
           <b-tag type="is-dark">version</b-tag>
-          <b-tag type="is-info">0.1.0</b-tag>
+          <b-tag type="is-info">0.1.1</b-tag>
         </b-taglist>
       </div>
     </form>
